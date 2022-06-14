637ccb633b35f70668c3b28caaf57b09c8ddb314
JiangShuuu <globelex65@gmail.com>
2022-06-14 10:42:56 +0800

    add dashboard

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
@@ -19,6 +20,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
