56098839692b3fbee023139d22b30518797290a6
JiangShuuu <globelex65@gmail.com>
2022-06-17 22:24:30 +0800

    upd commit

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,14 +3,16 @@ const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9 // 新增
+    const DEFAULT_LIMIT = 9
+
     const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1 // 新增
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 新增
-    const offset = getOffset(limit, page) // 新增
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
 
     return Promise.all([
-      Restaurant.findAndCountAll({ // 修改這裡
+      Restaurant.findAndCountAll({
         include: Category,
         where: {
           ...categoryId ? { categoryId } : {}
@@ -24,33 +26,36 @@ const restaurantController = {
     ])
       .then(([restaurants, categories]) => {
         const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-
         const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50),
           isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
+
         return res.render('restaurants', {
           restaurants: data,
           categories,
           categoryId,
-          pagination: getPagination(limit, page, restaurants.count) // 修改這裡，把 pagination 資料傳回樣板
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
         { model: Comment, include: User }
-      ],
-      nest: true,
-      raw: true
+      ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
