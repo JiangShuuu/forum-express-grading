d845e5c2c2eaece31a13b48bf7284c1924370a83
JiangShuuu <globelex65@gmail.com>
2022-06-23 11:30:36 +0800

    add signup

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+const userServices = require('../../services/user-services')
 const db = require('../../models')
 const { User, Restaurant, Comment, Favorite, Like, Followship } = db
 const { imgurFileHandler } = require('../../helpers/file-helper')
@@ -8,24 +8,11 @@ const userController = {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-
-    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加 return
-      })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        req.flash('success_messages', '成功註冊帳號！')
-        res.redirect('/signin')
-      })
-      .catch(err => next(err))
+    userServices.signUp(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', 'User was successfully created')
+      res.redirect('/signin')
+    })
   },
   signInPage: (req, res) => {
     res.render('signin')
