c53f5a508512e46f819b4d4dfd5444d6ebd5c7a6
JiangShuuu <globelex65@gmail.com>
2022-06-23 13:13:13 +0800

    add signup

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -4,7 +4,13 @@ const passport = require('../config/passport')
 //   return passport.authenticate('jwt', { session: false })(req, res, next)
 // }
 
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+    next()
+  })(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
